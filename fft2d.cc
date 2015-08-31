19681a0e9f003c4d10a218917a5d2f758729c732
Guru Das <gurooodas@gmail.com>
2015-08-31 13:58:52 -0400

    First row of image 1-D FFT matches with ref
    Also modified .gitignore for *.swp files

--- fft2d.cc
+++ fft2d.cc
@@ -1,6 +1,6 @@
 // Distributed two-dimensional Discrete FFT transform
-// YOUR NAME HERE
-// ECE8893 Project 1
+// Guru Das Srinagesh
+// ECE 6122 Project 1
 
 
 #include <iostream>
@@ -17,6 +17,40 @@
 
 using namespace std;
 
+void Transform1D(Complex* h, int w, Complex* H)
+{
+  // Implement a simple 1-d DFT using the double summation equation
+  // given in the assignment handout.  h is the time-domain input
+  // data, w is the width (N), and H is the output array.
+
+	int N = w; 		// For convenience's sake
+	int i;
+	
+	Complex W;		// This is the W in the formula
+	
+	Complex sum;	// Sum of all W[k]*h[k] inner loop products
+
+	/* Testing out this object thing */
+	//W.real = 3; W.imag = 4;
+	//cout<<"This complex number is "<<W<<endl;
+	
+	/* Build the 1-D transform formula */
+	int n, k;
+	cout<<"Starting 1-D transform computation"<<endl;
+
+	//for(i=0; i<N; i++)
+		for(n=0; n<N; n++){
+			sum.real = 0; sum.imag = 0;
+			for(k=0; k<N; k++){
+				W.real = cos(2*M_PI*n*k/N); W.imag = -sin(2*M_PI*n*k/N);
+				sum = sum + (W * h[k]);
+			}
+			H[n].real = sum.real;
+			H[n].imag = sum.imag;
+		}
+
+	cout<<"1-D transform computation complete"<<endl;
+}
 
 void Transform2D(const char* inputFN) 
 { // Do the 2D transform here.
@@ -37,17 +71,29 @@ void Transform2D(const char* inputFN)
   // 9) Send final answers to CPU 0 (unless you are CPU 0)
   //   9a) If you are CPU 0, collect all values from other processors
   //       and print out with SaveImageData().
+
+	/* Step 1:	Read in the image and find out width and height */
   InputImage image(inputFN);  // Create the helper object for reading the image
-  // Step (1) in the comments is the line above.
-  // Your code here, steps 2-9
+	int width = image.GetWidth();
+	int height = image.GetHeight();
+
+	/* Step 2:	Use MPI to determine how many CPUs there are, etc. */
+	//					As an initial step, forget about MPI, just get 1-D transform right
+
+	/* Step 3:	Create the H array that contains the 2-D DFT results */
+	Complex* H = new Complex[width * height];
+
+	/* Step 4:	Get the data of the input image, namely create the h array */
+	Complex* h = image.GetImageData();
+
+	/* Step 5:	Do 1-D transform of input image */
+	//					As an initial step, forget about MPI, just get 1-D transform right
+	Transform1D(h, 256/**256*/, H);
+
+	/* Step XX:	Save 1-D transform image data for debug */
+	image.SaveImageData("MyAfter1d.txt", H, width, height);
 }
 
-void Transform1D(Complex* h, int w, Complex* H)
-{
-  // Implement a simple 1-d DFT using the double summation equation
-  // given in the assignment handout.  h is the time-domain input
-  // data, w is the width (N), and H is the output array.
-}
 
 int main(int argc, char** argv)
 {
