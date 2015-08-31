1bc3492f13146bcfeac10da8eef6fff6a9e7cda3
Guru Das <gurooodas@gmail.com>
2015-08-31 14:50:56 -0400

    1-D transform for all rows complete and matching with ref
    All rows of the 1-D transform match with the ref now.

--- fft2d.cc
+++ fft2d.cc
@@ -23,33 +23,54 @@ void Transform1D(Complex* h, int w, Complex* H)
   // given in the assignment handout.  h is the time-domain input
   // data, w is the width (N), and H is the output array.
 
-	int N = w; 		// For convenience's sake
-	int i;
-	
-	Complex W;		// This is the W in the formula
-	
-	Complex sum;	// Sum of all W[k]*h[k] inner loop products
+  int N = w;    // For convenience's sake
+  int row;
+  
+  Complex W;    // This is the W in the formula
+  
+  Complex sum;  // Sum of all W[k]*h[k] inner loop products
 
-	/* Testing out this object thing */
-	//W.real = 3; W.imag = 4;
-	//cout<<"This complex number is "<<W<<endl;
-	
-	/* Build the 1-D transform formula */
-	int n, k;
-	cout<<"Starting 1-D transform computation"<<endl;
+  /* Testing out this object thing */
+  //W.real = 3; W.imag = 4;
+  //cout<<"This complex number is "<<W<<endl;
+  
+  /* Build the 1-D transform formula */
+  int n, k;
+  cout<<"Starting 1-D transform computation"<<endl;
 
-	//for(i=0; i<N; i++)
-		for(n=0; n<N; n++){
-			sum.real = 0; sum.imag = 0;
-			for(k=0; k<N; k++){
-				W.real = cos(2*M_PI*n*k/N); W.imag = -sin(2*M_PI*n*k/N);
-				sum = sum + (W * h[k]);
-			}
-			H[n].real = sum.real;
-			H[n].imag = sum.imag;
-		}
+ // for(n=0; n<N; n++){
+ //   sum.real = 0; sum.imag = 0;
+ //   for(k=0; k<N; k++){
+ //     W.real = cos(2*M_PI*n*k/N); W.imag = -sin(2*M_PI*n*k/N);
+ //     sum = sum + (W * h[k]);
+ //   }
+ //   H[n].real = sum.real;
+ //   H[n].imag = sum.imag;
+ // }
 
-	cout<<"1-D transform computation complete"<<endl;
+ // for(n=N; n<2*N; n++){
+ //   sum.real = 0; sum.imag = 0;
+ //   for(k=0; k<N; k++){
+ //     W.real = cos(2*M_PI*n*k/N); W.imag = -sin(2*M_PI*n*k/N);
+ //     sum = sum + (W * h[N+k]);
+ //   }
+ //   H[n].real = sum.real;
+ //   H[n].imag = sum.imag;
+ // }
+
+  for(row=0; row<N; row++)
+    for(n=N*row; n<N*(row+1); n++){
+      sum.real = 0; sum.imag = 0;
+      for(k=0; k<N; k++){
+        W.real = cos(2*M_PI*n*k/N); W.imag = -sin(2*M_PI*n*k/N);
+        sum = sum + (W * h[(N*row) + k]);
+      }
+      H[n].real = sum.real;
+      H[n].imag = sum.imag;
+    } 
+
+
+  cout<<"1-D transform computation complete"<<endl;
 }
 
 void Transform2D(const char* inputFN) 
@@ -72,26 +93,26 @@ void Transform2D(const char* inputFN)
   //   9a) If you are CPU 0, collect all values from other processors
   //       and print out with SaveImageData().
 
-	/* Step 1:	Read in the image and find out width and height */
+  /* Step 1:  Read in the image and find out width and height */
   InputImage image(inputFN);  // Create the helper object for reading the image
-	int width = image.GetWidth();
-	int height = image.GetHeight();
+  int width = image.GetWidth();
+  int height = image.GetHeight();
 
-	/* Step 2:	Use MPI to determine how many CPUs there are, etc. */
-	//					As an initial step, forget about MPI, just get 1-D transform right
+  /* Step 2:  Use MPI to determine how many CPUs there are, etc. */
+  //          As an initial step, forget about MPI, just get 1-D transform right
 
-	/* Step 3:	Create the H array that contains the 2-D DFT results */
-	Complex* H = new Complex[width * height];
+  /* Step 3:  Create the H array that contains the 2-D DFT results */
+  Complex* H = new Complex[width * height];
 
-	/* Step 4:	Get the data of the input image, namely create the h array */
-	Complex* h = image.GetImageData();
+  /* Step 4:  Get the data of the input image, namely create the h array */
+  Complex* h = image.GetImageData();
 
-	/* Step 5:	Do 1-D transform of input image */
-	//					As an initial step, forget about MPI, just get 1-D transform right
-	Transform1D(h, 256/**256*/, H);
+  /* Step 5:  Do 1-D transform of input image */
+  //          As an initial step, forget about MPI, just get 1-D transform right
+  Transform1D(h, 256/**256*/, H);
 
-	/* Step XX:	Save 1-D transform image data for debug */
-	image.SaveImageData("MyAfter1d.txt", H, width, height);
+  /* Step XX: Save 1-D transform image data for debug */
+  image.SaveImageData("MyAfter1d.txt", H, width, height);
 }
 
 
